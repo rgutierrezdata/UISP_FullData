09d72929d3d4df746018996b99f2a1dc3098ad34
Roberto Gutiérrez <rgutierrez@equaldata.com>
2022-02-02 15:10:37 -0500

    Query updated

--- src/routes/uisp/uisp.js
+++ src/routes/uisp/uisp.js
@@ -151,7 +151,7 @@ exports.getAllData = async() => {
     //SELECT TOP (1) * FROM [uispdb].[dbo].[uispData] ORDER BY uispdbID DESC
     //SELECT * FROM uispData WHERE uispdbID = 7008
     
-    sql.query(connectionString, 'SELECT * FROM uispData WHERE uispdbID = 715', (err, rows) => {
+    sql.query(connectionString, 'SELECT * FROM uispData WHERE uispdbID BETWEEN 1001 AND 1500', (err, rows) => {
 			if(err) {
 				logger.log('error',`Folder: uisp - File: uisp.js - Function_Name: getAllData - Error ${err}`);
 				//return reject(respose.responseFromServer().error.SYSTEM_ERROR);
