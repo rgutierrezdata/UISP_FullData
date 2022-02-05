d381b121445aec1701f0210f6c15587d91e8806f
Roberto Gutiérrez <rgutierrez@equaldata.com>
2022-02-05 10:19:02 -0500

    Clients from 2501 to 3000

--- src/routes/uisp/uisp.js
+++ src/routes/uisp/uisp.js
@@ -157,7 +157,7 @@ exports.getAllData = async() => {
     //SELECT TOP (1) * FROM [uispdb].[dbo].[uispData] ORDER BY uispdbID DESC
     //SELECT * FROM uispData WHERE uispdbID = 7008
     
-    sql.query(connectionString, 'SELECT * FROM uispData WHERE uispdbID BETWEEN 2001 AND 2500', (err, rows) => {
+    sql.query(connectionString, 'SELECT * FROM uispData WHERE uispdbID BETWEEN 2501 AND 3000', (err, rows) => {
 			if(err) {
 				logger.log('error',`Folder: uisp - File: uisp.js - Function_Name: getAllData - Error ${err}`);
 				//return reject(respose.responseFromServer().error.SYSTEM_ERROR);
